a436fe8465f05652c2954e5189f7c44c0ebd6abe
Brian Matthew Holder <60249613+Zanmore@users.noreply.github.com>
2020-06-16 13:26:17 -0500

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-function submitForm(){
-    console.log("I ran.");
-    document.getElementById("input").innerHTML = document.getElementById("eeInput").value;
-    document.getElementById("input").innerHTML = document.getElementById("checkInput").checked;
-} 
-
